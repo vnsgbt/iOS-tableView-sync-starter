9f6d2add41461986604e908dfe8224527c5a9bc7
steve <vnsgbt@me.com>
2016-03-14 13:16:34 -0500

    Add cell for tableView

---
+++ iOS-tableView-sync-starter/SyncTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SyncTableViewCell.swift
+//  iOS-tableView-sync-starter
+//
+//  Created by dev2 on 3/11/16.
+//  Copyright © 2016 vnsgbt. All rights reserved.
+//
+
+import UIKit
+
+class SyncTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
